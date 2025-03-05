1c7c10ba2c92f562af7962ef649fbf964dced85d
Yusuf <yusufparuk895@gmail.com>
2025-03-05 21:17:12 +0200

    SecondActivity updated

--- app/src/main/java/com/fake/welcometoandroid/SecondActivity.kt
+++ app/src/main/java/com/fake/welcometoandroid/SecondActivity.kt
@@ -1,6 +1,9 @@
 package com.fake.welcometoandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,19 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val welcomeText: TextView = findViewById(R.id.welcomeText)
+        val backButton: Button = findViewById(R.id.backButton)
+
+        // Get data from MainActivity
+        val userName = intent.getStringExtra("USER_NAME")
+        welcomeText.text = "Welcome, $userName!"
+
+        // Navigate back to MainActivity
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()  // Close current activity
         }
     }
 }
